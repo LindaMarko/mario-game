753a4de762b18e7738f6adc4470f82227d0701b6
Linda Marko <linda.marko@vaimo.com>
2023-09-21 07:07:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('./'));
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader(
     'Access-Control-Allow-Origin',
     'https://mario-game-lindamarko.vercel.app/'
@@ -11,7 +11,7 @@ app.use(express.static('./'));
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-}); */
+});
 
 const server = app.listen(8000, () => {
   const port = server.address().port;
